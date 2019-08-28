2e8bdf2010fc34c169e198ba6d86ae56ede5a9f6
Clementine <clementine.jager@ardata.fr>
2019-08-28 15:21:37 +0200

    emissions_data

--- R/data.R
+++ R/data.R
@@ -21,3 +21,99 @@
 "stations"
 
 # https://documenter.getpostman.com/view/2471055/RWaDWrU7?version=latest
+
+
+#' @title Emissions information for EPCI
+#' @description emissions information for epci stored in three
+#' data.frame. The dataset contains the
+#' following columns:
+#'
+#' * `gid`: group identifier
+#' * `lib_epci`: public intermunicipal cooperation institute name
+#' * `annee_inv`: year
+#' * `pm10`: Particulate matter with a diameter of 10 micrometers or less (in kg)
+#' * `pm25`: Particulate matter with a diameter of 25 micrometers or less (in kg)
+#' * `nox`: Nitrogen oxide (in kg)
+#' * `so2`: Sulfur dioxide (in kg)
+#' * `co`: Carbon monoxide (in kg)
+#' * `nh3`: Ammonia (in kg)
+#' * `covnm`: Non-methane volatile organic compounds (in kg)
+#' * `as`: Arsenic (in kg)
+#' * `cd`: Cadmium (in kg)
+#' * `ni`: Nickel (in kg)
+#' * `pb`: Lead (in kg)
+#' * `bap`: Benzo(a)pyrene (in kg)
+#' * `co2_t`: Carbon dioxide. Measure for this emission is tons.
+#'
+#' @docType data
+#' @author Airparif
+#' @references \url{https://data-airparif-asso.opendata.arcgis.com}
+#' @keywords data
+"data_emissions_epci"
+
+# https://documenter.getpostman.com/view/2471055/RWaDWrU7?version=latest
+
+
+#' @title Emissions information for departement
+#' @description emissions information for departement stored in three
+#' data.frame. The dataset contains the
+#' following columns:
+#'
+#' * `gid`: group identifier
+#' * `lib_dep`: township where the station is located
+#' * `code_dep`: township where the station is located
+#' * `annee_inv`: year
+#' * `pm10`: Particulate matter with a diameter of 10 micrometers or less (in kg)
+#' * `pm25`: Particulate matter with a diameter of 25 micrometers or less (in kg)
+#' * `nox`: Nitrogen oxide (in kg)
+#' * `so2`: Sulfur dioxide (in kg)
+#' * `co`: Carbon monoxide (in kg)
+#' * `nh3`: Ammonia (in kg)
+#' * `covnm`: Non-methane volatile organic compounds (in kg)
+#' * `as`: Arsenic (in kg)
+#' * `cd`: Cadmium (in kg)
+#' * `ni`: Nickel (in kg)
+#' * `pb`: Lead (in kg)
+#' * `bap`: Benzo(a)pyrene (in kg)
+#' * `co2_t`: Carbon dioxide. Measure for this emission is tons.
+#'
+#'
+#' @docType data
+#' @author Airparif
+#' @references \url{https://data-airparif-asso.opendata.arcgis.com}
+#' @keywords data
+"data_emissions_departement"
+
+# https://documenter.getpostman.com/view/2471055/RWaDWrU7?version=latest
+
+
+#' @title Emissions information for region
+#' @description emissions information for region stored in three
+#' data.frame. The dataset contains the
+#' following columns:
+#'
+#' * `gid`: group identifier
+#' * `lib_reg`: township where the station is located
+#' * `code_reg`: township where the station is located
+#' * `annee_inv`: township identifyer
+#' * `pm10`: Particulate matter with a diameter of 10 micrometers or less (in kg)
+#' * `pm25`: Particulate matter with a diameter of 25 micrometers or less (in kg)
+#' * `nox`: Nitrogen oxide (in kg)
+#' * `so2`: Sulfur dioxide (in kg)
+#' * `co`: Carbon monoxide (in kg)
+#' * `nh3`: Ammonia (in kg)
+#' * `covnm`: Non-methane volatile organic compounds (in kg)
+#' * `as`: Arsenic (in kg)
+#' * `cd`: Cadmium (in kg)
+#' * `ni`: Nickel (in kg)
+#' * `pb`: Lead (in kg)
+#' * `bap`: Benzo(a)pyrene (in kg)
+#' * `co2_t`: Carbon dioxide. Measure for this emission is tons.
+#'
+#' @docType data
+#' @author Airparif
+#' @references \url{https://data-airparif-asso.opendata.arcgis.com}
+#' @keywords data
+"data_emissions_region"
+
+# https://documenter.getpostman.com/view/2471055/RWaDWrU7?version=latest
